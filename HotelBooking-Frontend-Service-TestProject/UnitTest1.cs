19dad58f418bbd347cdfd08f9f7505e72bc9bf74
geethasamynathan <geethasamynathan2011@gmail.com>
2024-10-12 15:47:21 +0530

    Add project files.

---
+++ HotelBooking-Frontend-Service-TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HotelBooking_Frontend_Service_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
